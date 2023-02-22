da5ba918d9512309f6d0587729092825ac8c9191
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-22 10:35:10 +0300

    Change the program for command-line usage
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- CSharpChatGPT/Program.cs
+++ CSharpChatGPT/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No argument has been given!!!");
+        }
+        else
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+        
     }
 }
